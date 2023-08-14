c741cc75296b91b78be2a5c7cd4314deb65df41f
fuyongjie <fuyongjie@shuidi-inc.com>
2023-08-14 14:37:30 +0800

    test

--- vue3-ts-study/src/views/AboutView.vue
+++ vue3-ts-study/src/views/AboutView.vue
@@ -1,8 +1,25 @@
+<!--
+ * @Author: fuyongjie
+ * @Date: 2023-08-09 21:44:09
+ * @Description: 文件描述~
+ * @FilePath: /vue3-study/vue3-ts-study/src/views/AboutView.vue
+ * @LastEditTime: 2023-08-14 12:06:53
+ * @LastEditors: fuyongjie
+-->
 <template>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute()
+const router = useRouter()
+
+console.log('dddd', route.params === router.currentRoute.value.params)
+</script>
 
 <style>
 @media (min-width: 1024px) {
